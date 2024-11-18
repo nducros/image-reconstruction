34b3863aca4eac5b2151bf08adbfb1837af3b9af
nducros <nicolas.ducros@insa-lyon.fr>
2024-11-18 21:46:36 +0100

    forward operator and ART recon

--- lesson.py
+++ lesson.py
@@ -140,3 +140,166 @@ for f in filter_list:
     plt.savefig(save_folder / f'{image_file.stem}_{f}.pdf',
                 bbox_inches='tight')
     plt.close()
+    
+    
+#%% Create forward operator 
+from skimage.transform import rescale
+
+img_size = 32 # we assume image is square
+n_angle = 90
+n_detec = 45
+
+# Init matrix A
+A = np.zeros((n_detec*n_angle, img_size*img_size)) #COMLPLETE
+
+# Build the forward operator, one column at a time. 
+theta = np.linspace(0.0, 180.0, n_angle+1)[:-1]
+
+for i in range(img_size):
+    for j in range(img_size):
+        # Activating a single pixel of the object image
+        image = np.zeros((img_size,img_size)) ##COMPLETE
+        image[i,j] = 1 ##COMLPETE
+        
+        # Radon transform
+        sinogram = radon(image, theta, circle=False) ##COMPLETE
+        sinogram = rescale(sinogram, scale=(n_detec/sinogram.shape[0],1), mode='reflect')
+        
+        # Concatenating results in matrix A 
+        A[:,img_size*i+j] = np.reshape(sinogram, (n_detec*n_angle, ))
+
+# A matrix visualisation
+fig, ax = plt.subplots()
+ax.imshow(A)
+ax.set_title("A")
+ax.set_ylabel(r"Projection ray $(r, \theta)$")
+ax.set_xlabel(r"Image pixel $x$")
+plt.show()
+
+
+#%% Check forward operator
+from skimage.data import shepp_logan_phantom
+
+phantom = shepp_logan_phantom()
+phantom = rescale(phantom, scale=(img_size/phantom.shape[0]), mode='reflect')
+
+# Radon transform with skimage function
+sinog = radon(phantom, theta, circle=False) ## COMPLETE
+sinog = rescale(sinog, scale=(n_detec/sinog.shape[0], 1), mode='reflect') ##COMPLETE
+
+# Radon transform as a matrix-vector product ## COMPLETE
+f = np.reshape(phantom, (-1, 1))
+m = A @ f
+sinog2 = np.reshape(m, (n_detec, n_angle)) 
+
+# Plots ##COMPLETE THE PLOT
+fig, (ax1, ax2) = plt.subplots(1, 2, ) #figsize=(8, 4.5)
+ax1.set_title(r"Sinogram from" + "\nRadon function")
+ax1.set_xlabel(r"Projection angle $\theta$ (in deg)")
+ax1.set_ylabel(r"Projection position $r$ (in pixels)")
+ax1.imshow(sinog, cmap=plt.cm.Greys_r, extent=(0, 180, 0, sinog.shape[0]), aspect='auto')
+
+ax2.set_title(r"Sinogram from" + "\n" + r"forward matrix $A$")
+ax2.set_xlabel(r"Projection angle $\theta$ (in deg)")
+ax2.set_ylabel(r"Projection position $r$ (in pixels)")
+ax2.imshow(sinog2, cmap=plt.cm.Greys_r, extent=(0, 180, 0, sinog2.shape[0]), aspect='auto')
+
+fig.tight_layout()
+
+#%% Algebraic reconstruction technique
+
+def art(A, m, n_ite=15, f0=np.zeros((A.shape[1],1)), gamma=1):
+    """
+    Algebraic reconstruction technique.
+    
+    It solve the linear system :math:`m = Af` in an iterative manner. It is 
+    known as Kaczmarz method in numerical linear algebra.
+       
+
+    Args:
+        A (ndarray): System matrix :math:`A`.
+        
+        m (ndarray): Measurement vector :math:`m`.
+        
+        f0 (ndarray, optional): Initialisation. Defaults to 0.
+        
+        n_ite (int, optional): Number of iterations. Defaults to 15.
+        
+        gamma (float, optional): Relaxation parameter in )0,1). Defaults to 1.
+
+    Returns:
+        f (ndarray): Unknown vector :math:`f`.
+    """
+     
+    # remove unrelevant (rho, theta) rays
+    a = np.sum(A, 1, keepdims=True)
+    ind = np.nonzero(a)[0]
+    m = m[ind]
+    A = A[ind,:]
+    
+    f = f0
+    # External iteration loop
+    for kk in range(n_ite):     
+        # Loop over the rows of A 
+        for ii in range(A.shape[0]): 
+            a_m = A[ii,:]
+            a_m = a_m[:,None] 
+            f = f - gamma*(a_m.T @ f - m[ii])*a_m/(a_m.T @ a_m)
+    
+    return f, gamma 
+
+#%% Reconstruction using ART
+n_ite = 20
+
+f_, _  = art(A, m, I=n_ite)            # 10 iterations
+f2_, _ = art(A, m, I=n_ite, f0 = f_)   # 10 more iterations
+
+f  = f_.reshape((img_size,img_size))
+f2 = f2_.reshape((img_size,img_size))    
+
+
+# Display results
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4.5))
+ax1.set_title(r"Ground Truth")
+ax1.imshow(phantom, cmap=plt.cm.Greys_r)
+
+ax2.set_title(f"ART ({n_ite} iterations)")
+ax2.imshow(f, cmap=plt.cm.Greys_r)
+
+ax3.set_title(f"ART ({2*n_ite} iterations)")
+ax3.imshow(f2, cmap=plt.cm.Greys_r)
+ 
+#%% Solve
+import scipy.linalg as lin
+import time
+
+# Use the computed sinogram
+sinogram = np.reshape(sinog, (-1, 1))
+
+# Compute the pseudoinverse
+t0 = time.perf_counter()
+pinv = lin.pinv(A) # COMPLETE
+t0 = time.perf_counter() - t0
+
+# Reconstruct with pseudoinverse 
+t1 = time.perf_counter()
+rec_pi = np.reshape(np.dot(pinv, sinogram), (img_size,img_size)) # COMPLETE
+t1 = time.perf_counter() - t1
+print(f'Recon with pseudoinverse: {t0:.3f} + {t1:.4f} s')
+
+# Reconstruct with a linear solver
+t2 = time.perf_counter()
+rec_solv = np.reshape(lin.lstsq(A, sinogram)[0], (img_size,img_size)) # COMPLETE
+t2 = time.perf_counter() - t2
+print(f'Recon with solver: {t2:.3f} s')
+
+# Display results
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4.5))
+ax1.set_title(r"Ground Truth")
+ax1.imshow(phantom, cmap=plt.cm.Greys_r)
+
+ax2.set_title(r"Recon with pseudoinverse")
+ax2.imshow(rec_pi, cmap=plt.cm.Greys_r)
+
+ax3.set_title(r'Recon with solver')
+ax3.imshow(rec_solv, cmap=plt.cm.Greys_r)
